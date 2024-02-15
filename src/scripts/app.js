d45a63f4d4001004a65b8346ad4dbbdbc6bb798b
LouisePrd <louise.peridy22@gmail.com>
2024-02-15 23:11:06 +0100

    new design

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -5,17 +5,11 @@ function anim(htmlelement, prompt) {
     if (cmpt < prompt.length) {
       htmlelement.innerHTML += prompt.charAt(cmpt);
       cmpt++;
-    } else {
-      setTimeout(function () {
-        document.getElementsByClassName("star")[0].style.display = "flex";
-        clearInterval();
-      }, 300);
     }
   }, 120);
 }
 
 window.onload = function () {
-  document.getElementsByClassName("star")[0].style.display = "none";
   const htmlelement = document.getElementsByTagName("h1")[0];
   const prompt = "Creative portfolio";
   anim(htmlelement, prompt);
