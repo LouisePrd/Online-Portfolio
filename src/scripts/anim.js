f80d6ef07c0e6dc31aaea6c81bf4621ff08cab19
LouisePrd <louise.peridy22@gmail.com>
2024-07-04 19:23:10 +0200

    new anim inspos

--- src/scripts/anim.js
+++ src/scripts/anim.js
@@ -5,22 +5,39 @@ window.onload = function () {
 };
 
 function show() {
-  i = 0;
-  setInterval(function () {
-    const images = document.getElementsByTagName("img");
-    images[i].style.visibility = "visible";
-    i++;
-  }, 500);
+  let i = 0;
+  const images = document.getElementsByTagName("img");
+  const interval = setInterval(function () {
+    if (i < images.length) {
+      images[i].style.visibility = "visible";
+      const randomAngle = Math.random() * 30 - 15; // Génère un angle entre -15 et 15 degrés
+      images[i].style.transform = `rotate(${randomAngle}deg)`;
+      i++;
+    } else {
+      clearInterval(interval);
+      resetImages(); // Appeler la fonction pour remettre les images droites
+    }
+  }, 400);
+}
+
+function resetImages() {
+  const images = document.getElementsByTagName("img");
+  setTimeout(function () {
+    for (let img of images) {
+      img.style.transform = 'rotate(0deg)';
+    }
+  }, 1000);
 }
 
 function anim(htmlelement, prompt) {
   let cmpt = 0;
 
-  setInterval(function () {
+  const interval = setInterval(function () {
     if (cmpt < prompt.length) {
       htmlelement.innerHTML += prompt.charAt(cmpt);
       cmpt++;
     } else {
+      clearInterval(interval);
       show();
     }
   }, 100);
