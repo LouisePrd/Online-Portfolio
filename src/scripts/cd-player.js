d83ef41d2e4a216dfad9ed7c9f685596dc810122
LouisePrd <louise.peridy22@gmail.com>
2024-07-05 22:00:07 +0200

    update page categories

---
+++ src/scripts/cd-player.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", function () {
+  cdPlayer = document.getElementsByClassName("cd-player")[0];
+  cd = document.getElementsByClassName("cd")[0];
+  
+
+  cdPlayer.addEventListener("click", function () {
+    if (cd.classList.contains("play")) {
+      cd.classList.remove("play");
+      cd.classList.add("pause");
+      cdPlayer.style.transform = "rotate(180deg)";
+    } else {
+      cd.classList.remove("pause");
+      cd.classList.add("play");
+      cdPlayer.style.transform = "rotate(140deg)";
+    }
+  });
+});
